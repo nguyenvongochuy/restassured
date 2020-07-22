0fa91069be531523a702c2b1a99937ac4404544b
nguyenvongochuy <nguyenvongochuy@gmail.com>
2020-07-21 23:26:01 -0500

    initial version

---
+++ src/main/java/com/basic/GetMapTest.java
@@ -0,0 +1,79 @@
+package com.basic;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+//Note:
+// given(): I have this information
+// when(): I perform action
+// then(): I need to check output
+
+
+public class GetMapTest {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://maps.googleapis.com";
+        RestAssured.basePath = "/map/api";
+
+    }
+
+    @Test(enabled = false)
+    public void statusCodeTest() {
+        given()
+                .param("units", "imperial")
+                .param("origins", "Washington,DC")
+                .param("destinations", "New+York+City,NY")
+                .param("key", "abczaSyAFNxOzcDNEZ9")
+        .when()
+                .get("/distancematrix/json")
+        .then()
+                .statusCode(200);
+
+    }
+
+    @Test
+    public void responseTest() {
+        Response response = given()
+                .param("units", "imperial")
+                .param("origins", "Washington,DC")
+                .param("destinations", "New+York+City,NY")
+                .param("key", "abczaSyAFNxOzcDNEZ9")
+        .when()
+                .get("/distancematrix/json")
+        .then()
+                .statusCode(404)
+                .extract()
+                .response();
+
+        System.out.println("Print response: " + response.body().prettyPrint());
+
+
+    }
+
+    @Test
+    public void responseTest1() {
+        given()
+                .param("units", "imperial")
+                .param("origins", "Washington,DC")
+                .param("destinations", "New+York+City,NY")
+                .param("key", "abczaSyAFNxOzcDNEZ9")
+        .when()
+                .get("/distancematrix/json")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("row[0].element[0].distance.text", equalTo("255 mi"));
+
+
+
+    }
+
+}
